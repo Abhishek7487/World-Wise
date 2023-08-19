f4653fda5137c88ac9f8ae790b63b9fb67f1d1f7
Abhishek7487 <dellabhishek7487@gmail.com>
2023-08-19 16:36:26 +0530

    Using Css modules and added starter files

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,12 +1,14 @@
 import { Link } from "react-router-dom";
 import PageNav from "../components/PageNav";
+import AppNav from "../components/AppNav";
 
 function Homepage() {
   return (
     <div>
       <PageNav />
+      <AppNav />
       <h1>WorldWise</h1>
-      <Link to="/pricing">Pricing</Link>
+      <Link to="/app">Go to the App</Link>
     </div>
   );
 }
