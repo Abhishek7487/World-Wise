2de47598450cd22ee560562191fe64b5537b648c
Abhishek7487 <dellabhishek7487@gmail.com>
2023-08-22 11:07:28 +0530

    Reading and Setting Query String

--- src/App.jsx
+++ src/App.jsx
@@ -10,6 +10,7 @@ import AppLayout from "./pages/AppLayout";
 import CityList from "./components/CityList";
 import CountryList from "./components/CountryList";
 import City from "./components/City";
+import Form from "./components/Form";
 
 const BASE_URL = "http://localhost:9000";
 
@@ -54,7 +55,7 @@ function App() {
             path="countries"
             element={<CountryList cities={cities} isLoding={isLoading} />}
           />
-          <Route path="form" element={<p>Form</p>} />
+          <Route path="form" element={<Form />} />
         </Route>
         <Route path="*" element={<PageNotFound />} />
       </Routes>
--- src/components/City.jsx
+++ src/components/City.jsx
@@ -1,5 +1,5 @@
-import { useParams } from "react-router-dom";
 import styles from "./City.module.css";
+import { useSearchParams } from "react-router-dom";
 
 const formatDate = (date) =>
   new Intl.DateTimeFormat("en", {
@@ -10,6 +10,11 @@ const formatDate = (date) =>
   }).format(new Date(date));
 
 function City() {
+  const [searchParams, setSearchParams] = useSearchParams();
+
+  const lat = searchParams.get("lat");
+  const lng = searchParams.get("lng");
+
   // TEMP DATA
   const currentCity = {
     cityName: "Lisbon",
@@ -20,7 +25,15 @@ function City() {
 
   const { cityName, emoji, date, notes } = currentCity;
 
-  return <h1>City</h1>;
+  return (
+    <>
+      <h1>City</h1>
+      <p>
+        Position: {lat}, {lng}
+      </p>
+      ;
+    </>
+  );
 
   // return (
   //   <div className={styles.city}>
